f98a4e80a69a14a6a10c321d49568dcf6fb08cac
Andrias Meisyal <andriasonline@gmail.com>
2016-08-10 08:52:24 +0700

    Add "Hello World!" translator

--- lib/sastrawi.rb
+++ lib/sastrawi.rb
@@ -1,7 +1,23 @@
 require "sastrawi/version"
 
 module Sastrawi
-  def self.hi
-    puts "Hello world!"
+  def self.hi(language = "english")
+    translator = Translator.new(language)
+    translator.hi
+  end
+end
+
+class Sastrawi::Translator
+  def initialize(language)
+    @language = language
+  end
+
+  def hi
+    case @language
+    when "indonesia"
+      "Halo dunia!"
+    else
+      "Hello world!"
+    end
   end
 end
