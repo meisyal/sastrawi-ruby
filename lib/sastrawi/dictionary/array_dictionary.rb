2c1679990b1049a322fbc6f5662304a39c303754
Andrias Meisyal <andriasonline@gmail.com>
2016-08-12 16:39:50 +0700

    Fix dictionary module

--- lib/sastrawi/dictionary/array_dictionary.rb
+++ lib/sastrawi/dictionary/array_dictionary.rb
@@ -1,25 +1,27 @@
 module Sastrawi
-  class ArrayDictionary
-    attr_accessor :words
+  module Dictionary
+    class ArrayDictionary
+      attr_accessor :words
 
-    def initialize
-      self.words = []
-    end
+      def initialize
+        self.words = []
+      end
 
-    def lookup(word)
-      puts word if words.include?(word)
-    end
+      def lookup(word)
+        puts word if words.include?(word)
+      end
 
-    def count()
-      puts words.length
-    end
+      def count()
+        puts words.length
+      end
 
-    def add_words(words)
-      # TODO: Add an array to current array.
-    end
+      def add_words(words)
+        # TODO: Add an array to current array.
+      end
 
-    def add(word)
-      words.push(word)
+      def add(word)
+        words.push(word)
+      end
     end
   end
 end
