dfe3b448c8096a93dfd0ed5f772c001b52cfd587
Andrias Meisyal <andriasonline@gmail.com>
2016-08-14 11:58:09 +0700

    Add "Stemmer" class

---
+++ lib/sastrawi/stemmer/stemmer.rb
@@ -0,0 +1,33 @@
+module Sastrawi
+  module Stemmer
+    class Stemmer
+      def initialize
+        # TODO: Implement this method here.
+      end
+
+      def get_dictionary
+        # TODO: Implement this method here.
+      end
+
+      def stem(text)
+        # TODO: Implement this method here.
+      end
+
+      def stem_word(word)
+        # TODO: Implement this method here.
+      end
+
+      def plural?(word)
+        # TODO: Implement this method here.
+      end
+
+      def stem_plural_word(word)
+        # TODO: Implement this method here.
+      end
+
+      def stem_singular_word(word)
+        # TODO: Implement this method here.
+      end
+    end
+  end
+end
