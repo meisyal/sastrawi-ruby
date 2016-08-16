ef13deea8a51ebc74465d0ca2d5409f1b5a6772c
Andrias Meisyal <andriasonline@gmail.com>
2016-08-16 07:04:29 +0700

    Add "StemmerFactory" class

---
+++ lib/sastrawi/stemmer/stemmer_factory.rb
@@ -0,0 +1,21 @@
+module Sastrawi
+  module Stemmer
+    class StemmerFactory
+      def create_stemmer
+        # TODO: Implement this method here.
+      end
+
+      def create_default_dictionary
+        # TODO: Implement this method here.
+      end
+
+      def get_words
+        # TODO: Implement this method here.
+      end
+
+      def get_words_from_file
+        # TODO: Implement this method here.
+      end
+    end
+  end
+end
