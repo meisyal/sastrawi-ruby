bf6ae35fffc019186dd03db7bc5b89f8df61e249
Andrias Meisyal <andriasonline@gmail.com>
2016-08-17 11:00:04 +0700

    Add "CachedStemmer" class

---
+++ lib/sastrawi/stemmer/cached_stemmer.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module Stemmer
+    class CachedStemmer
+      def initialize
+      end
+
+      def stem(text)
+      end
+
+      def get_cache
+      end
+
+      def get_delegated_stemmer
+      end
+    end
+  end
+end
