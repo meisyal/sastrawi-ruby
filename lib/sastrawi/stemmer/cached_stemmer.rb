bb474f9fab0ae94eac4500e40f751b0b0c946035
Andrias Meisyal <andriasonline@gmail.com>
2016-08-17 17:34:05 +0700

    Add TODO comment

--- lib/sastrawi/stemmer/cached_stemmer.rb
+++ lib/sastrawi/stemmer/cached_stemmer.rb
@@ -2,15 +2,19 @@ module Sastrawi
   module Stemmer
     class CachedStemmer
       def initialize
+        # TODO: Implement this method here.
       end
 
       def stem(text)
+        # TODO: Implement this method here.
       end
 
       def get_cache
+        # TODO: Implement this method here.
       end
 
       def get_delegated_stemmer
+        # TODO: Implement this method here.
       end
     end
   end
