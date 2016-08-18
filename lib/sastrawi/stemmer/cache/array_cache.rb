9f457f0d9c4b71b971a0c1add4b9b83b4cb268b4
Andrias Meisyal <andriasonline@gmail.com>
2016-08-18 09:53:44 +0700

    Add "ArrayCache" class

---
+++ lib/sastrawi/stemmer/cache/array_cache.rb
@@ -0,0 +1,23 @@
+module Sastrawi
+  module Stemmer
+    module Cache
+      class ArrayCache
+        def initialize
+          # TODO: Implement this method here.
+        end
+
+        def set
+          # TODO: Implement this method here.
+        end
+
+        def get
+          # TODO: Implement this method here.
+        end
+
+        def has(key)
+          # TODO: Implement this method here.
+        end
+      end
+    end
+  end
+end
