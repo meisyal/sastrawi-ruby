a7efee917d1ad372a6cdb2037a4a8a7da09db359
Andrias Meisyal <andriasonline@gmail.com>
2016-08-19 08:26:11 +0700

    Add "TextNormalizer" class

---
+++ lib/sastrawi/stemmer/filter/text_normalizer.rb
@@ -0,0 +1,11 @@
+module Sastrawi
+  module Stemmer
+    module Filter
+      class TextNormalizer
+        def self.normalize_text(text)
+          # TODO: Implement this method here.
+        end
+      end
+    end
+  end
+end
