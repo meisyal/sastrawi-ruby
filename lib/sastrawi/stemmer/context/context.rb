9529fc28d193bf2deefa3caf0bf0c792bec02db1
Andrias Meisyal <andriasonline@gmail.com>
2016-08-22 08:18:55 +0700

    Add "Context" class

---
+++ lib/sastrawi/stemmer/context/context.rb
@@ -0,0 +1,55 @@
+module Sastrawi
+  module Stemmer
+    module Context
+      class Context
+        def initialize
+          # TODO: Implement this method here.
+        end
+
+        def init_visitors
+          # TODO: Implement this method here.
+        end
+
+        def stop_process
+          # TODO: Implement this method here.
+        end
+
+        def add_removal
+          # TODO: Implement this method here.
+        end
+
+        def execute
+          # TODO: Implement this method here.
+        end
+
+        def start_stemming_process
+          # TODO: Implement this method here.
+        end
+
+        def remove_prefixes
+          # TODO: Implement this method here.
+        end
+
+        def remove_suffixes
+          # TODO: Implement this method here.
+        end
+
+        def accept
+          # TODO: Implement this method here.
+        end
+
+        def accept_visitors
+          # TODO: Implement this method here.
+        end
+
+        def accept_prefix_visitors
+          # TODO: Implement this method here.
+        end
+
+        def suffix_removal?
+          # TODO: Implement this method here.
+        end
+
+        def restore_prefix
+          # TODO: Implement this method here.
+        end
