7d9653e181b8ceb06e9bae509c3cf855c46d5c59
Andrias Meisyal <andriasonline@gmail.com>
2016-08-26 09:06:36 +0700

    Add "InvalidAffixPairSpecification" class

---
+++ lib/sastrawi/morphology/invalid_affix_pair_specification.rb
@@ -0,0 +1,9 @@
+module Sastrawi
+  module Morphology
+    class InvalidAffixPairSpecification
+      def satisfied_by?(word)
+        # TODO: Implement this method here.
+      end
+    end
+  end
+end
