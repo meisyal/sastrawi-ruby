76d05b33c5804683c431b01eca28ba6a2f601b7d
Andrias Meisyal <andriasonline@gmail.com>
2016-08-27 10:00:36 +0700

    Add "StopWordRemover" class

---
+++ lib/sastrawi/stop_word_remover/stop_word_remover.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module StopWordRemover
+    class StopWordRemover
+      def initialize
+        # TODO: Implement this method here.
+      end
+
+      def get_dictionary
+        # TODO: Implement this method here.
+      end
+
+      def remove(text)
+        # TODO: Implement this method here.
+      end
+    end
+  end
+end
