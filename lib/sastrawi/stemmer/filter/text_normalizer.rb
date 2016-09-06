90ab47f8e355ec49f306c68881291042146beaa3
Andrias Meisyal <andriasonline@gmail.com>
2016-09-06 19:55:22 +0700

    Implement "normalize_text" function

--- lib/sastrawi/stemmer/filter/text_normalizer.rb
+++ lib/sastrawi/stemmer/filter/text_normalizer.rb
@@ -3,7 +3,11 @@ module Sastrawi
     module Filter
       class TextNormalizer
         def self.normalize_text(text)
-          # TODO: Implement this method here.
+          lowercase_text = text.downcase
+          replaced_text = lowercase_text.gsub!(/[^a-z0-9 -]/im, ' ')
+          replaced_text = replaced_text.gsub!(/( +)/im, ' ')
+
+          replaced_text.strip!
         end
       end
     end
