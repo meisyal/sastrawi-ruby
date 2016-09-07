f3e873cc3c86b484b5f4550ff869c2c23d75f56c
Andrias Meisyal <andriasonline@gmail.com>
2016-09-07 19:16:17 +0700

    Implement "satisfied_by?" function

--- lib/sastrawi/stemmer/confix_stripping/precedence_adjustment_specification.rb
+++ lib/sastrawi/stemmer/confix_stripping/precedence_adjustment_specification.rb
@@ -3,7 +3,13 @@ module Sastrawi
     module ConfixStripping
       class PrecedenceAdjustmentSpecification
         def satisfied_by?(value)
-          # TODO: Implement this method here.
+          regex_rules = ['/^be(.*)lah$/', '/^be(.*)an$/', '/^me(.*)i$/', '/^di(.*)i$/', '/^pe(.*)i$/', '/^ter(.*)i$/']
+
+          regex_rules.each do |r|
+            return true if r.gsub!(regex_rules, value)
+          end
+
+          false
         end
       end
     end
