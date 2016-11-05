fb45361d0001f9899465f464346b34181df9c265
Andrias Meisyal <andriasonline@gmail.com>
2016-11-05 11:06:48 +0700

    Implement "satisfied_by?" function

--- lib/sastrawi/morphology/invalid_affix_pair_specification.rb
+++ lib/sastrawi/morphology/invalid_affix_pair_specification.rb
@@ -2,7 +2,22 @@ module Sastrawi
   module Morphology
     class InvalidAffixPairSpecification
       def satisfied_by?(word)
-        # TODO: Implement this method here.
+        return false if /^me(.*)kan$/.match(word)
+
+        return false if word == 'ketahui'
+
+        invalid_affixes = [
+          /^ber(.*)i$/, /^di(.*)an$/, /^ke(.*)i$/, /^ke(.*)an$/,
+          /^me(.*)an$/, /^me(.*)an$/, /^ter(.*)an$/, /^per(.*)an$/
+        ]
+
+        contains = false
+
+        invalid_affixes.each do |invalid_affix|
+          contains = contains || invalid_affix.match(word)
+        end
+
+        return contains
       end
     end
   end
