17dbb81d018eb79a782397461a29ff2780d60478
Andrias Meisyal <andriasonline@gmail.com>
2016-11-06 11:58:23 +0700

    Implement "ArrayCache" class

--- lib/sastrawi/stemmer/cache/array_cache.rb
+++ lib/sastrawi/stemmer/cache/array_cache.rb
@@ -2,20 +2,22 @@ module Sastrawi
   module Stemmer
     module Cache
       class ArrayCache
+        attr_accessor :data
+
         def initialize
-          # TODO: Implement this method here.
+          @data = Hash.new
         end
 
-        def set
-          # TODO: Implement this method here.
+        def set(key, value)
+          @data[key] = value
         end
 
-        def get
-          # TODO: Implement this method here.
+        def get(key)
+          return @data[key] if @data.has_key?(key.to_sym)
         end
 
         def has(key)
-          # TODO: Implement this method here.
+          return key if @data.has_key?(key.to_sym)
         end
       end
     end
