ea7a33d1eed4a4821073f4b84e86bc06435bc211
Andrias Meisyal <andriasonline@gmail.com>
2016-11-12 11:43:02 +0700

    Remove "Translator" class

--- lib/sastrawi.rb
+++ lib/sastrawi.rb
@@ -1,9 +1,5 @@
 require "sastrawi/version"
-require "sastrawi/translator"
 
 module Sastrawi
-  def self.hi(language = "english")
-    translator = Translator.new(language)
-    translator.hi
-  end
+  # TODO: Main section of sastrawi
 end
