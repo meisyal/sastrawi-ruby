78a1bcc85230e8cc513fb5525fa3026f93094367
Andrias Meisyal <andriasonline@gmail.com>
2016-12-02 08:44:31 +0700

    Add implementation of ninth rule of disambiguator prefix

---
+++ lib/sastrawi/morphology/disambiguator/disambiguator_prefix_rule9.rb
@@ -0,0 +1,19 @@
+module Sastrawi
+  module Morphology
+    module Disambiguator
+      class DisambiguatorPrefixRule9
+        def disambiguate(word)
+          contains = /^te([bcdfghjklmnpqrstvwxyz])er(([bcdfghjklmnpqrstvwxyz]).*)$/.match(word)
+
+          if contains
+            matches = contains.captures
+
+            return if matches[0] == 'r'
+
+            return matches[0] << 'er' << matches[1] << matches[2]
+          end
+        end
+      end
+    end
+  end
+end
