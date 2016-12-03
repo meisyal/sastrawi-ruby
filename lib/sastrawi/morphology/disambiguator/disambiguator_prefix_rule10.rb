c8e03e3a1171660ccdfe0539646d568980f5dd32
Andrias Meisyal <andriasonline@gmail.com>
2016-12-03 08:45:03 +0700

    Add implementation of tenth rule of disambiguator prefix

---
+++ lib/sastrawi/morphology/disambiguator/disambiguator_prefix_rule10.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module Morphology
+    module Disambiguator
+      class DisambiguatorPrefixRule10
+        def disambiguate(word)
+          contains = /^me([lrwy])([aiueo])(.*)$/.match(word)
+
+          if contains
+            matches = contains.captures
+
+            return matches[0] << matches[1] << matches[2]
+          end
+        end
+      end
+    end
+  end
+end
