b5d299d17a43801bf6a77ec1c2aa23a88a7f6365
Andrias Meisyal <andriasonline@gmail.com>
2016-12-07 08:18:36 +0700

    Add implementation of fourteenth rule of disambiguator prefix

---
+++ lib/sastrawi/morphology/disambiguator/disambiguator_prefix_rule14.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module Morphology
+    module Disambiguator
+      class DisambiguatorPrefixRule14
+        def disambiguate(word)
+          contains = /^men([cdjstz])(.*)$/.match(word)
+
+          if contains
+            matches = contains.captures
+
+            return matches[0] << matches[1]
+          end
+        end
+      end
+    end
+  end
+end
