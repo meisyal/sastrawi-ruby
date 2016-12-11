2b639b221c6a2bd2d5e8b913c359ff466481a66e
Andrias Meisyal <andriasonline@gmail.com>
2016-12-11 09:46:41 +0700

    Add implementation of eighteenth rule of disambiguator prefix

---
+++ lib/sastrawi/morphology/disambiguator/disambiguator_prefix_rule18b.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module Morphology
+    module Disambiguator
+      class DisambiguatorPrefixRule18a
+        def disambiguate(word)
+          contains = /^meny([aiueo])(.*)$/.match(word)
+
+          if contains
+            matches = contains.captures
+
+            return 's' << matches[0] << matches[1]
+          end
+        end
+      end
+    end
+  end
+end
