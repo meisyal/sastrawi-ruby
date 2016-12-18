53d3c3ac3816aa37b65fa010ac6fffcd07a986ed
Andrias Meisyal <andriasonline@gmail.com>
2016-12-19 06:45:11 +0700

    Add implementation of twenty-seventh rule of disambiguator prefix

---
+++ lib/sastrawi/morphology/disambiguator/disambiguator_prefix_rule27.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module Morphology
+    module Disambiguator
+      class DisambiguatorPrefixRule27
+        def disambiguate(word)
+          contains = /^pen([cdjstz])(.*)$/.match(word)
+
+          if contains
+            matches = contains.captures
+
+            return matches[0] << matches[1]
+          end
+        end
+      end
+    end
+  end
+end
