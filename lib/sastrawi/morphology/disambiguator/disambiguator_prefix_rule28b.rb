331c9d3bee89879f3e6f44925d993f3bd17c19fd
Andrias Meisyal <andriasonline@gmail.com>
2016-12-20 09:02:58 +0700

    Add implementation of twenty-eighth rule of disambiguator prefix

---
+++ lib/sastrawi/morphology/disambiguator/disambiguator_prefix_rule28b.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module Morphology
+    module Disambiguator
+      class DisambiguatorPrefixRule28b
+        def disambiguate(word)
+          contains = /^pen([aiueo])(.*)$/.match(word)
+
+          if contains
+            matches = contains.captures
+
+            return 't' << matches[0] << matches[1]
+          end
+        end
+      end
+    end
+  end
+end
