5c5b2a8c7391aebac91edd0681019f994f7e5930
Andrias Meisyal <andriasonline@gmail.com>
2016-12-23 17:46:13 +0700

    Add implementation of thirtieth-first rule of disambiguator prefix

---
+++ lib/sastrawi/morphology/disambiguator/disambiguator_prefix_rule31b.rb
@@ -0,0 +1,17 @@
+module Sastrawi
+  module Morphology
+    module Disambiguator
+      class DisambiguatorPrefixRule31b
+        def disambiguate(word)
+          contains = /^peny([aiueo])(.*)$/.match(word)
+
+          if contains
+            matches = contains.captures
+
+            return 's' << matches[0] << matches[1]
+          end
+        end
+      end
+    end
+  end
+end
