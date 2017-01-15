7a9bb4965fb8b3475989bb66976fef7b2bd56f2f
Andrias Meisyal <andriasonline@gmail.com>
2017-01-15 10:08:26 +0700

    Don't use bang for gsub and split methods

--- lib/sastrawi/stemmer/filter/text_normalizer.rb
+++ lib/sastrawi/stemmer/filter/text_normalizer.rb
@@ -4,10 +4,10 @@ module Sastrawi
       class TextNormalizer
         def self.normalize_text(text)
           lowercase_text = text.downcase
-          replaced_text = lowercase_text.gsub!(/[^a-z0-9 -]/im, ' ')
-          replaced_text = replaced_text.gsub!(/( +)/im, ' ')
+          replaced_text = lowercase_text.gsub(/[^a-z0-9 -]/im, ' ')
+          replaced_text = replaced_text.gsub(/( +)/im, ' ')
 
-          replaced_text.strip!
+          replaced_text.strip
         end
       end
     end
