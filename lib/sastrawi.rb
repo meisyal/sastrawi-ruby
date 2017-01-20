f4a99d04c776a91e9c7bd5f8152d7924238196e8
Andrias Meisyal <andriasonline@gmail.com>
2017-01-20 18:13:55 +0700

    Add interface to stem words

--- lib/sastrawi.rb
+++ lib/sastrawi.rb
@@ -1,5 +1,12 @@
-require "sastrawi/version"
+require 'sastrawi/version'
+
+require 'sastrawi/stemmer/stemmer_factory'
 
 module Sastrawi
-  # TODO: Main section of sastrawi
+  def self.stem(sentence)
+      stemmer_factory = Sastrawi::Stemmer::StemmerFactory.new
+      stemmer = stemmer_factory.create_stemmer
+
+      puts stemmer.stem(sentence)
+  end
 end
