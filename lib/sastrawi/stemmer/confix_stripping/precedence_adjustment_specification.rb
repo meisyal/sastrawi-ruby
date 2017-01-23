6af9ad6873b884a4cc0f0d9745ccd3721cd3199d
Andrias Meisyal <andriasonline@gmail.com>
2017-01-23 08:56:43 +0700

    Remove apostrophe in array of regular expressions

--- lib/sastrawi/stemmer/confix_stripping/precedence_adjustment_specification.rb
+++ lib/sastrawi/stemmer/confix_stripping/precedence_adjustment_specification.rb
@@ -4,8 +4,8 @@ module Sastrawi
       class PrecedenceAdjustmentSpecification
         def satisfied_by?(value)
           regex_rules = [
-            '/^be(.*)lah$/', '/^be(.*)an$/', '/^me(.*)i$/',
-            '/^di(.*)i$/', '/^pe(.*)i$/', '/^ter(.*)i$/'
+            /^be(.*)lah$/, /^be(.*)an$/, /^me(.*)i$/,
+            /^di(.*)i$/, /^pe(.*)i$/, /^ter(.*)i$/
           ]
 
           regex_rules.each do |rule|
