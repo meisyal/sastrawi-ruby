7768ecb87aa3cfa8fec27703f4eafbcef1fee0af
Andrias Meisyal <andriasonline@gmail.com>
2017-01-26 23:04:14 +0700

    Fix implementation of "StopWordRemover" class
    Changes:
    - Replace get_dictionary method via attribute accessor.
    - Change "not if" with "unless".
    - Use join to concat string in array.

--- lib/sastrawi/stop_word_remover/stop_word_remover.rb
+++ lib/sastrawi/stop_word_remover/stop_word_remover.rb
@@ -1,14 +1,10 @@
 module Sastrawi
   module StopWordRemover
     class StopWordRemover
-      attr_reader :dictionary
+      attr_accessor :dictionary
 
       def initialize(dictionary)
         @dictionary = dictionary
-      end
-
-      def get_dictionary
-        @dictionary
       end
 
       def remove(text)
@@ -16,12 +12,12 @@ module Sastrawi
         stop_words = []
 
         words.each do |word|
-          if !@dictionary.include?(word)
+          unless @dictionary.include?(word)
             stop_words.push(word)
           end
         end
 
-        return ' ' << stop_words
+        stop_words.join(' ')
       end
     end
   end
