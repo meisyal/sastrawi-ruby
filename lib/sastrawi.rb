59035095c1497b5a56ae2fef5664f68e140a3194
Andrias Meisyal <andriasonline@gmail.com>
2017-02-12 22:33:17 +0700

    Use two spaces for default indentation

--- lib/sastrawi.rb
+++ lib/sastrawi.rb
@@ -4,9 +4,9 @@ require 'sastrawi/stemmer/stemmer_factory'
 
 module Sastrawi
   def self.stem(sentence)
-      stemmer_factory = Sastrawi::Stemmer::StemmerFactory.new
-      stemmer = stemmer_factory.create_stemmer
+    stemmer_factory = Sastrawi::Stemmer::StemmerFactory.new
+    stemmer = stemmer_factory.create_stemmer
 
-      stemmer.stem(sentence)
+    stemmer.stem(sentence)
   end
 end
