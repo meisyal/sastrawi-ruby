9b686bf4a0388faf3254c82ffccfcbb1d75edd9b
Andrias Meisyal <andriasonline@gmail.com>
2017-02-13 12:04:01 +0700

    Add test for short words and "nilai"

--- spec/sastrawi_spec.rb
+++ spec/sastrawi_spec.rb
@@ -5,7 +5,19 @@ describe Sastrawi do
     expect(Sastrawi::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'should not stem short words' do
+    short_words = %w[mei bui]
+    base_form = %w[mei bui]
+    stemming_result = []
+
+    short_words.each do |word|
+      stemming_result.push(Sastrawi.stem(word))
+    end
+
+    expect((base_form - stemming_result).empty?).to eq(true)
+  end
+
+  it 'should not stem "nilai" to "nila"' do
+    expect(Sastrawi.stem('nilai')).to eq('nilai')
   end
 end
