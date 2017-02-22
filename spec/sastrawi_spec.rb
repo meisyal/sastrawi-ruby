d04b3d09b88c1c2eefb6b7b212dfd7ebbe144b66
Andrias Meisyal <andriasonline@gmail.com>
2017-02-22 21:54:12 +0700

    Add test for "-lah, -kah, -tah, -pun" suffixes

--- spec/sastrawi_spec.rb
+++ spec/sastrawi_spec.rb
@@ -20,4 +20,16 @@ describe Sastrawi do
   it 'should not stem "nilai" to "nila"' do
     expect(Sastrawi.stem('nilai')).to eq('nilai')
   end
+
+  it 'should stem "-lah, -kah, -tah, -pun" suffixes' do
+    suffixed_words = %w[hancurlah benarkah apatah siapapun]
+    base_form = %w[hancur benar apa siapa]
+    stemming_result = []
+
+    suffixed_words.each do |word|
+      stemming_result.push(Sastrawi.stem(word))
+    end
+
+    expect((base_form - stemming_result).empty?).to eq(true)
+  end
 end
