79fd04d37f076522b843066dc60c899eccffc6cc
Andrias Meisyal <andriasonline@gmail.com>
2017-03-22 15:22:51 +0700

    Remove "stem" method

--- lib/sastrawi.rb
+++ lib/sastrawi.rb
@@ -1,12 +1,3 @@
 require 'sastrawi/version'
 
 require 'sastrawi/stemmer/stemmer_factory'
-
-module Sastrawi
-  def self.stem(sentence)
-    stemmer_factory = Sastrawi::Stemmer::StemmerFactory.new
-    stemmer = stemmer_factory.create_stemmer
-
-    stemmer.stem(sentence)
-  end
-end
